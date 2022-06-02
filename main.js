452bb16397b258bc188b35a651d8d7cc31780190
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-02 04:24:55 +0100

    finally gotten a method for the light & dark mode

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-
 let side = document.querySelector("aside");
 
-document.getElementById("menu").addEventListener("click", ()=>{
-    side.style.display = "block";
+document.getElementById("menu").addEventListener("click", () => {
+  side.style.display = "block";
 });
-document.getElementById("closebtn").addEventListener("click", ()=> {
-    side.style.display = "none";
+document.getElementById("closebtn").addEventListener("click", () => {
+  side.style.display = "none";
 });
 
-const theme = document.querySelector(".toggler");
-theme.addEventListener("click", ()=>{
-    document.body.classList.toggle("themeVariables");
-})
+/* ------theme ------ */
+let theme = document.querySelector(".toggler");
+theme.addEventListener("click", () => {
+  document.body.classList.toggle("themeVariables");
+});
