26005170f16d89516fb802c70ad48e710e14147c
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-02 03:38:36 +0100

    changed my js to arrow function

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-function display(){
-    document.getElementById("aside").style.display = "inline-block";
-}
-function Close(){
-    document.getElementById("aside").style.display = "none";
-}
+
+let side = document.querySelector("aside");
+
+document.getElementById("menu").addEventListener("click", ()=>{
+    side.style.display = "block";
+});
+document.getElementById("closebtn").addEventListener("click", ()=> {
+    side.style.display = "none";
+});
