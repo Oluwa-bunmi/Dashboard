284593364234a3cc9433e5e1d53863e213da173f
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-03 03:38:33 +0100

    done, light and dark mode remaining

--- index.js
+++ index.js
@@ -13,29 +13,27 @@ themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("themeVariables");
   themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
-
 });
-
-
-
-
-
-
-
 
 // Fill orders in table
 
-Orders.forEach(order => {
-    const tr = document.createElement('tr');
-    const trContent = `
+Orders.forEach((order) => {
+  const tr = document.createElement("tr");
+  const trContent = `
     
                             <td>${order.productName}</td>
                             <td>${order.productNumber}</td>
                             <td>${order.paymentStatus}</td>
-                            <td class="warning">${order.shipping}</td>
+                            <td class="warning ${
+                              order.shipping === "Declined"
+                                ? "danger"
+                                : order.shipping === "Pending"
+                                ? "warning"
+                                : "primary"
+                            }">${order.shipping}</td>
                             <td class="primary">Details</td>
                             `;
-                             
-    tr.innerHTML = trContent;
-    document.querySelector('table tbody').appendChild(tr);                        
+
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
 });
