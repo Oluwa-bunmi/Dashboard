e28bfb9b88e28a6f5b1c0a38f4ed647c526d63d9
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-03 17:49:52 +0100

    trying to get a way around the themes

--- index.js
+++ index.js
@@ -8,12 +8,24 @@ document.getElementById("closebtn").addEventListener("click", () => {
 });
 
 /* ------theme toggler for light and dark mode ------ */
-let themeToggler = document.querySelector(".toggler");
-themeToggler.addEventListener("click", () => {
-  document.body.classList.toggle("themeVariables");
-  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
-  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
-});
+// let themeToggler = document.querySelector(".toggler");
+// themeToggler.addEventListener("click", () => {
+//   document.body.classList.toggle("themeVariables");
+//   themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+//   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+// });
+let moon = document.getElementById("moon");
+function themefunction(){
+  
+}
+
+
+
+
+
+
+
+
 
 // Fill orders in table
 
