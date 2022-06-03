c8174d49c8f4bfe951c050d4c436db1fba898417
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-03 01:38:48 +0100

    moving on...trial by error

--- main.js
+++ main.js
@@ -11,4 +11,7 @@ document.getElementById("closebtn").addEventListener("click", () => {
 let themeToggler = document.querySelector(".toggler");
 themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("themeVariables");
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+
 });
